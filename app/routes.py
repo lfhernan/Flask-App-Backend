29db53c622a094619ed6faa7a0284ab92c3d3bc9
Luis Hernandez <luisfhernandez415@gmail.com>
2020-11-23 23:31:11 -0500

    Added streaming service

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,18 @@
-from flask import send_from_directory
+from flask import send_from_directory, Response
 from flask_cors import cross_origin
 from app import app
 from app.auth import requires_auth, requires_scope
+from livestream.stream import generate
 
 @app.route('/api/video/<path:path>')
 @cross_origin(headers=["Content-Type", "Authorization"])
 @requires_auth
 def get_video(path):
-    return send_from_directory('../assets', path)
+	return send_from_directory('../assets', path)
 
 @app.route('/api/livestream')
 @cross_origin(headers=["Content-Type", "Authorization"])
 @requires_auth
-def get_livestream():
-	return 'Success!'
+def video_feed():
+	# return the response generated along with the specific media type (mime type)
+	return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
